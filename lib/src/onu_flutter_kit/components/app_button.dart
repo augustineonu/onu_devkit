bfd372f811607e354fd2b36f298907c28cd0afcf
Augustine <97133584+augustineonu@users.noreply.github.com>
2026-03-11 15:16:08 +0100

    initial commit

---
+++ lib/src/onu_flutter_kit/components/app_button.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:drugstoc_mobile/core/theme/app_colors.dart' show AppColors;
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final VoidCallback? onPressed;
+  final bool isLoading;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final Color? loadingColor;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final double borderRadius;
+  final double? width;
+  final double? height;
+  final EdgeInsetsGeometry? padding;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final Color? borderColor;
+
+  const CustomButton({
+    super.key,
+    required this.title,
+    this.onPressed,
+    this.isLoading = false,
+    this.backgroundColor,
+    this.textColor,
+    this.loadingColor,
+    this.fontSize,
+    this.fontWeight,
+    this.borderRadius = 40,
+    this.width,
+    this.height,
+    this.padding,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.borderColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDisabled = onPressed == null || isLoading;
+    final Color resolvedBg = backgroundColor ?? AppColors.primaryLight;
+    final Color resolvedText = textColor ?? Colors.white;
+
+    return GestureDetector(
+      onTap: isDisabled ? null : onPressed,
+      child: Container(
+        width: width,
+        height: height ?? 50.h,
+        padding:
+            padding ?? EdgeInsets.symmetric(vertical: 10.h, horizontal: 24.w),
+        decoration: BoxDecoration(
+          color: isDisabled ? resolvedBg.withValues(alpha: 0.6) : resolvedBg,
+          borderRadius: BorderRadius.circular(borderRadius),
+          border: borderColor != null ? Border.all(color: borderColor!) : null,
+        ),
+        child: isLoading
+            ? Center(
+                child: SizedBox(
+                  height: 20.h,
+                  width: 20.h,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2.5,
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      loadingColor ?? resolvedText,
+                    ),
+                  ),
+                ),
+              )
+            : Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (prefixIcon != null) ...[
+                    prefixIcon!,
+                    SizedBox(width: 8.w),
+                  ],
+                  Flexible(
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: fontSize ?? 16.sp,
+                        fontWeight: fontWeight ?? FontWeight.w600,
+                        color: isDisabled
+                            ? resolvedText.withValues(alpha: 0.6)
+                            : resolvedText,
+                      ),
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ),
+                  if (suffixIcon != null) ...[
+                    SizedBox(width: 8.w),
+                    suffixIcon!,
+                  ],
+                ],
+              ),
+      ),
+    );
+  }
+}
